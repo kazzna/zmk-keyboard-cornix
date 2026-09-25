c7b4d2c405c3b6c188d2959a116b58029c12b959
kazzna <hykathon@gmail.com>
2026-09-25 23:00:03 +0900

    Synchronise layer identifiers, display names and constants with specification

---
+++ config/includes/layers.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2020 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+#define LAYER_BASE 0
+#define LAYER_SYMBOLS 1
+#define LAYER_NEOVIM 2
+#define LAYER_NAGINATA 3
+#define LAYER_NAGINATA_EXT 4
+#define LAYER_NAGINATA_SYMBOLS 5
+#define LAYER_NAVIGATION 6
+#define LAYER_SYSTEM 7
+#define LAYER_ARN_MOD 8
